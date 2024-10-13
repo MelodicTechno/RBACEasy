e33944b7245433cde6f97d8917911a19225196af
MelodicTechno <617179025@qq.com>
2024-10-13 13:25:28 +0800

    add getresource

--- src/access/AccessHandler.java
+++ src/access/AccessHandler.java
@@ -3,26 +3,28 @@ package src.access;
 import src.level.Level;
 import src.resource.Resource;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class AccessHandler {
 
     private final Level level;
-    private ArrayList<Resource> resources;
+    private List<Resource> resources;
     private final ArrayList<Path> resourcePaths;
-
+    private final String resourceRoot;
 
     public AccessHandler(Level level) {
         this.level = level;
         this.resourcePaths = new ArrayList<>();
+        this.resources = new ArrayList<>();
+        String rootPath = "files";
+        this.resourceRoot = String.format("%s/%s/", rootPath, level);
     }
 
     public void getAccess() {
@@ -37,7 +39,7 @@ public class AccessHandler {
         return level;
     }
 
-    public void updateResources() {
+    public void updateResourcePaths() {
         String resourcePath = "files/" + level + '/';
         try (Stream<Path> paths = Files.walk(Paths.get(resourcePath))) {
             paths.filter(Files::isRegularFile)
@@ -47,8 +49,37 @@ public class AccessHandler {
         }
     }
 
-    public ArrayList<Resource> getResources() {
-        return this.resources;
+    public void updateResources() {
+        List<Resource> resources = new ArrayList<>();
+        File folder = new File(this.resourceRoot);
+
+        if (folder.exists() && folder.isDirectory()) {
+            File[] files = folder.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    if (file.isFile() && file.getName().endsWith(".ser")) {
+                        Resource resource = this.loadResource(file);
+                        if (resource != null) {
+                            resources.add(resource);
+                        }
+                    }
+                }
+            }
+        } else {
+            System.err.println("The specified folder does not exist or is not a directory.");
+        }
+
+        this.resources = resources;
+    }
+
+    private Resource loadResource(File file) {
+        try (FileInputStream fileIn = new FileInputStream(file);
+             ObjectInputStream objectIn = new ObjectInputStream(fileIn)) {
+            return (Resource) objectIn.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            System.err.println("Error loading resource from file: " + file.getName() + ". Error: " + e.getMessage());
+            return null;
+        }
     }
 
     public ArrayList<Path> getResourcePaths() {
@@ -63,6 +94,10 @@ public class AccessHandler {
         } catch (IOException e) {
             System.err.println("Error saving the resource object: " + e.getMessage());
         }
+    }
+
+    public Resource getResource(String filePath) {
+        return null;
     }
 
 }
