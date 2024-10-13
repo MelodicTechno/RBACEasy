ea839afdbbd8d1200a8a80111c44cb73970dc99b
MelodicTechno <617179025@qq.com>
2024-10-13 12:59:40 +0800

    add save merchanics

--- src/access/AccessHandler.java
+++ src/access/AccessHandler.java
@@ -3,16 +3,26 @@ package src.access;
 import src.level.Level;
 import src.resource.Resource;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.stream.Stream;
 
 public class AccessHandler {
 
     private final Level level;
     private ArrayList<Resource> resources;
+    private ArrayList<Path> resourcePaths;
+
 
     public AccessHandler(Level level) {
         this.level = level;
+        this.resourcePaths = new ArrayList<>();
     }
 
     public void getAccess() {
@@ -26,4 +36,29 @@ public class AccessHandler {
     public Level getLevel() {
         return level;
     }
+
+    public void updateResources() {
+        String resourcePath = "files/" + level + '/';
+        try (Stream<Path> paths = Files.walk(Paths.get(resourcePath))) {
+            paths.filter(Files::isRegularFile) // 只筛选文件
+                    .forEach(path -> resourcePaths.add(path.getFileName())); // 将文件名添加到列表
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public ArrayList<Resource> getResources() {
+        return this.resources;
+    }
+
+    public static void saveResource(Resource resource, String filePath) {
+        try (FileOutputStream fileOut = new FileOutputStream(filePath);
+             ObjectOutputStream objectOut = new ObjectOutputStream(fileOut)) {
+            objectOut.writeObject(resource); // 将对象写入文件
+            System.out.println("Resource object has been saved.");
+        } catch (IOException e) {
+            System.err.println("Error saving the resource object: " + e.getMessage());
+        }
+    }
+
 }
--- src/resource/Resource.java
+++ src/resource/Resource.java
@@ -2,18 +2,25 @@ package src.resource;
 
 import src.level.Level;
 
+import java.io.*;
 import java.util.ArrayList;
+import java.util.UUID;
 
-public class Resource {
+public class Resource implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
     private final String resourceName;
-    private long resourceId;
+    private final UUID resourceId;
     private String resourcePath;
     Level level;
 
-    public Resource(String resourceName, String resourcePath, Level level) {
+    public Resource(String resourceName, Level level) {
         this.resourceName = resourceName;
-        this.resourcePath = resourcePath;
         this.level = level;
+        String rootPath = "files";
+        this.resourceId = UUID.randomUUID();
+        this.resourcePath = String.format("%s/%s/%s", rootPath, level, this.resourceId);
     }
 
     public String getResourceName() {
@@ -22,5 +29,20 @@ public class Resource {
 
     public void setResourcePath(String resourcePath) {
         this.resourcePath = resourcePath;
+    }
+
+    public UUID getResourceId() {
+        return this.resourceId;
+    }
+
+    public void getResource() {
+        try (BufferedReader br = new BufferedReader(new FileReader(this.resourcePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println("no resource found");
+        }
     }
 }
